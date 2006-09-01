b610119588d16132cd4c8fe2421474c0eebe4757
Suraj N. Kurapati <sunaku@gmail.com>
2006-08-31 18:02:53 -0700

    [project @ c2df24a5ac3227f8eb6f68e9e1201707a58dc9ee]
    [project @ 34]
    add forgotten copyright notice

--- Wmii.rb
+++ Wmii.rb
@@ -1,6 +1,5 @@
 =begin
   Copyright 2006 Suraj N. Kurapati
-  Copyright 2006 Stephan Maka
 
   This program is free software; you can redistribute it and/or
   modify it under the terms of the GNU General Public License
