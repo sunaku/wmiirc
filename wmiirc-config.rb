59daad1c50c38c0fef9bc2daf1f878e441f86b82
Suraj N. Kurapati <sunaku@gmail.com>
2008-01-27 11:25:17 -0800

    split status bar information into independent buttons

--- wmiirc-config.rb
+++ wmiirc-config.rb
@@ -176,31 +176,40 @@ EOF
     end
   end
 
-  action :status do
-    if defined? @status
-      @status.kill
-    end
+  class StatusBar < Thread
+    def initialize aBarNode, aRefreshRate, aBarColor = Color::NORMAL, &aBarText
+      raise ArgumentError unless block_given?
 
-    @status = Thread.new do
-      bar = fs.rbar.status
-      bar.create unless bar.exist?
+      super aBarNode do |b|
+        b.create unless b.exist?
 
-      loop do
-        diskSpace = `df -h ~`.split[-3..-1].join(' ')
-        cpuLoad = File.read('/proc/loadavg').split[0..2].join(' ')
-
-        5.times do
-          bar.write [
-            Color::NORMAL,
-            Time.now,
-            cpuLoad,
-            diskSpace,
-          ].join(' | ')
-
-          sleep 1
+        while true
+          b.write "#{aBarColor} #{aBarText.call}"
+          sleep aRefreshRate
         end
       end
     end
+  end
+
+  action :status do
+    if defined? @statusBars
+      @statusBars.each {|s| s.kill }
+    end
+
+    @statusBars = [
+      StatusBar.new(fs.rbar.clock, 1) do
+        Time.now
+      end,
+
+      StatusBar.new(fs.rbar.cpu_load, 5) do
+        File.read('/proc/loadavg').split[0..2].join(' ')
+      end,
+
+      StatusBar.new(fs.rbar.disk_space, 10) do
+        rem, use, dir = `df -h ~`.split[-3..-1]
+        "#{dir} #{use} used #{rem} free"
+      end,
+    ]
   end
 
 
