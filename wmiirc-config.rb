b9813a834f4b41a35489679c4865c58b5ae2fc93
Suraj N. Kurapati <sunaku@gmail.com>
2009-01-19 23:25:14 -0800

    make firefox windows open on the current view

--- wmiirc-config.rb
+++ wmiirc-config.rb
@@ -136,6 +136,26 @@ EOF
     end
   end
 
+  # wmii puts subsequent firefox instances on the same
+  # view as the first instance. bypass this and move
+  # the newly created firefox to the current view
+  #
+  # this "feature" applies to all programs
+  # that provide window grouping hints. so
+  # we'll just have this event handler
+  # bypass the feature for ALL programs! :-(
+  event :CreateClient do |id|
+    c = Client.new(id)
+
+    case c.label.read
+    when "Gran Paradiso - Restore Previous Session"
+      c.tags = 'web'
+    when /Firefox|Gran Paradiso|jEdit/i
+      c.tags = curr_tag
+      c.focus
+    end
+  end
+
 
 # Actions
 
