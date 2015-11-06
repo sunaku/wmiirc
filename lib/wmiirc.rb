ad5de83b379a8fe9015b7da0f16469305f2aaeda
Suraj N. Kurapati <sunaku@riseup.net>
2015-11-06 15:09:47 -0800

    add colors to log messages based on severity

--- lib/wmiirc.rb
+++ lib/wmiirc.rb
@@ -10,6 +10,19 @@ module Wmiirc
   # keep a log file to aid the user in debugging
   LOG = Logger.new(File.join(DIR, 'wmiirc.log'))
 
+  # add colors to log messages based on severity
+  fmt = Logger::Formatter.new
+  LOG.formatter = proc do |severity, datetime, progname, msg|
+    color = case severity
+            when 'DEBUG' then "\e[1;36m"       # bold cyan
+            when 'INFO'  then "\e[1;32m"       # bold green
+            when 'WARN'  then "\e[1;33m"       # bold yellow
+            when 'ERROR' then "\e[1;31m"       # bold red
+            when 'FATAL' then "\e[1;30m\e[41m" # bold black on red
+            end
+    [color, fmt.call(severity, datetime, progname, "\e[0m#{msg}")].join
+  end
+
   # insulation for code in user's configuration
   class Sandbox
     include Rumai
